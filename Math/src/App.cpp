7742a21b6e57a17c2bf49d42f86c8e73a1881094
Sky101223 <lixxlilol@163.com>
2025-03-15 19:59:36 +0800

    添加消息弹窗

--- Math/src/App.cpp
+++ Math/src/App.cpp
@@ -3,23 +3,32 @@
 #include <conio.h>
 #include <stdint.h>
 
+#include <Windows.h>
+
 #include "Base.h"
 #include "MathFunction.h"
 #include "Draw.hpp"
 
 int main(void)
 {
-    initgraph(WIDTH, HEIGHT, EX_SHOWCONSOLE);
+    initgraph(WIDTH, HEIGHT);
 
     // 建立平面直角坐标其XoY
     drawAxis();
 
     // 绘制函数
-    drawMathFunction(linearFunction, -0.232, -9);
-    drawMathFunction(quadraticFunction, 0.025, -1.3, 3.3209);
-    drawMathFunction(inverseProportionalFunction, 1000);
+    drawMathFunction(linearFunction, -0.232, -9); // 一次函数
+    drawMathFunction(quadraticFunction, 0.025, -1.3, 3.3209); // 二次函数
+    drawMathFunction(inverseProportionalFunction, 1000); // 反比例函数
 
-    _getch();
+    // 消息窗口
+    MessageBox(
+        NULL,
+        _T("所有函数已绘制成功！点击“确认”后，窗口会自动关闭"),
+        _T("完成"),
+        MB_OK | MB_ICONEXCLAMATION
+    );
     closegraph();
+
     return 0;
 }
